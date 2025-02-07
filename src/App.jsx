c064660178b3baa7aae4f67946c21cc38b2e0613
Александр <aleksandrkostin7890@gmail.com>
2025-02-07 21:47:02 +0200

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import "./App.scss";
+import "./scss/main.scss";
+import Dropdown from "./components/BaseComponents/Dropdown";
 
-import './App.scss'
-import './scss/main.scss'
+const App = () => {
+    return (
+        <div style={{ padding: "20px" }}>
+            <h1>Выберите опцию:</h1>
+            <Dropdown
+                options={["Выберите", "Опция 1", "Опция 2", "Опция 3"]}
+                onSelect={(value) => console.log("Вы выбрали:", value)}
+            />
+        </div>
+    );
+};
 
-function App() {
-
-
-  return (
-   <div>1234</div>
-  )
-}
-
-export default App
+export default App;
